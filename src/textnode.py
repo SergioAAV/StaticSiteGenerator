9c641cac2ae41f01e6610646a7bfda999319757b
SergioAAV <sergioaavreg@gmail.com>
2026-07-02 23:56:54 -0300

    Adding inline markdown

--- src/textnode.py
+++ src/textnode.py
@@ -43,3 +43,4 @@ def text_node_to_html_node(text_node: "TextNode") -> LeafNode:
             return LeafNode("img", "", {"src": text_node.url, "alt": text_node.text})
         case _:
             raise ValueError(f"invalid text type: {text_node.text_type}")
+        
